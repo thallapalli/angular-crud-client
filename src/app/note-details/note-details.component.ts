f5bf2d474f5b47da3a14554b40892ac1622b9506
karnakar <karnakar@ymail.com>
2019-12-14 22:28:04 -0500

    adding files

---
+++ src/app/note-details/note-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-details',
+  templateUrl: './note-details.component.html',
+  styleUrls: ['./note-details.component.styl']
+})
+export class NoteDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
