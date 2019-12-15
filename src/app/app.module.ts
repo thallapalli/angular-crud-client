1a03a38db754e1e851323c12d4a9ebe6f518fdaa
karnakar <karnakar@ymail.com>
2019-12-14 22:26:59 -0500

    commit initial compments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteDetailsComponent,
+    NoteListComponent,
+    CreateNoteComponent
   ],
   imports: [
     BrowserModule,
